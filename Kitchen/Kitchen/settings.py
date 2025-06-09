5c237b7371f093e146f57a200bbd9f8a71186dad
harif <harifn786@gmail.com>
2025-06-09 13:03:35 +0300

    updated requirements.txt

--- Kitchen/Kitchen/settings.py
+++ Kitchen/Kitchen/settings.py
@@ -26,7 +26,7 @@ load_dotenv(dotenv_path = dotenv_path)
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%oo2+m6awf+pt*_agka7gxnizvgkhm!nty92y4h2oe_)*sdi66'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
